c6f7ab652bb67386f130c2fa534089afb7898442
Ahmed hammad <73841142+ahmad3ssam@users.noreply.github.com>
2025-09-11 23:56:05 +0300

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -17,12 +17,12 @@ void	*ft_memset(void *s, int c, size_t n)
 	size_t			i;
 	unsigned char	*ptr;
 
-	ptr = s;
+	ptr = (unsigned char *)s;
 	i = 0;
 	while (i < n && ptr[i])
 	{
-		ptr[i] = c;
+		ptr[i] = (unsigned char)c;
 		i++;
 	}
-	return (ptr);
+	return (s);
 }
