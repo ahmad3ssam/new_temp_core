59ecde8c5dc1543f16bc7c0e1359a800758227e5
Ahmed hammad <73841142+ahmad3ssam@users.noreply.github.com>
2025-09-11 23:50:00 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -19,8 +19,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t			i;
 
 	i = 0;
-	ptr_src = (void *)src;
-	ptr_dest = dest;
+	ptr_src = (unsigned char *)src;
+	ptr_dest = (unsigned char *)dest;
 	while (ptr_src[i] != '\0' && i < n)
 	{
 		ptr_dest[i] = ptr_src[i];
